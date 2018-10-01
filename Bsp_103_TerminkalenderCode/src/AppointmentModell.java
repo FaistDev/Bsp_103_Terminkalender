42efa5add1b8f4a31897cd82ce1b31ba8e67e16c
zitmab16 <zitmab16@htl-kaindorf.ac.at>
2018-10-01 11:15:10 +0200

    Added Basic Functions from the BL-Model

--- Bsp_103_TerminkalenderCode/src/AppointmentModell.java
+++ Bsp_103_TerminkalenderCode/src/AppointmentModell.java
@@ -1,13 +1,23 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 
-/**
- *
- * @author Ben
- */
-public class AppointmentModell {
+import java.util.ArrayList;
+import javax.swing.AbstractListModel;
+
+
+public class AppointmentModell extends AbstractListModel {
+    private ArrayList<Appointment> appointments = new ArrayList<>();
+    
+    @Override
+    public int getSize() {
+      return appointments.size();
+    }
+
+    @Override
+    public Object getElementAt(int index) {
+        return appointments.get(index);
+    }
+    public void add(Appointment a){
+        appointments.add(a);
+        fireIntervalAdded(this, appointments.size()-1, appointments.size()-1);
+    }
     
 }

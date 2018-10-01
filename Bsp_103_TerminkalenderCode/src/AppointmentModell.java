4cbbbd6625c036d1efb8921f8fb595a4fddd0135
zitmab16 <zitmab16@htl-kaindorf.ac.at>
2018-10-01 11:49:26 +0200

    added functions from AppointmentModel

--- Bsp_103_TerminkalenderCode/src/AppointmentModell.java
+++ Bsp_103_TerminkalenderCode/src/AppointmentModell.java
@@ -1,3 +1,7 @@
+
+import java.util.ArrayList;
+import javax.swing.AbstractListModel;
+
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
@@ -8,7 +12,17 @@
  *
  * @author Ben
  */
-public class AppointmentModell {
+public class AppointmentModell extends AbstractListModel{
+    private ArrayList<Appointment> appointments = new ArrayList<>();
+    @Override
+    public int getSize() {
+        return appointments.size();
+    }
+
+    @Override
+    public Object getElementAt(int index) {
+        return appointments.get(index);
+    }
     
    
     

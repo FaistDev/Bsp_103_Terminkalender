6fc3b73b0ee7455d9daa0bbe9cac7bdfae53a581
zitmab16 <zitmab16@htl-kaindorf.ac.at>
2018-10-01 11:32:11 +0200

    merged

--- Bsp_103_TerminkalenderCode/src/Appointment.java
+++ Bsp_103_TerminkalenderCode/src/Appointment.java
@@ -1,8 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
 
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 
-
+/**
+ *
+ * @author Ben
+ */
 public class Appointment {
   private LocalDateTime dateTime;
   private String text;
--- Bsp_103_TerminkalenderCode/src/AppointmentModell.java
+++ Bsp_103_TerminkalenderCode/src/AppointmentModell.java
@@ -1,24 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
 
-import java.util.ArrayList;
-import javax.swing.AbstractListModel;
-
-
-public class AppointmentModell extends AbstractListModel {
-    private ArrayList<Appointment> appointments = new ArrayList<>();
-    
-    @Override
-    public int getSize() {
-      return appointments.size();
-    }
-
-    @Override
-    public Object getElementAt(int index) {
-        return appointments.get(index);
-    }
-    public void add(Appointment a){
-        appointments.add(a);
-        fireIntervalAdded(this, appointments.size()-1, appointments.size()-1);
-    }
+/**
+ *
+ * @author Ben
+ */
+public class AppointmentModell {
     
    
     
